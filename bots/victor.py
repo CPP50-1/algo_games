039083fc006cdead6c80cc31be326d1758da88f6
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-13 14:48:13 +0200

    feat : tron_bot V3
    avoid direct contact with all enemies + better avoidance of single cell paths
    cleanup of distance comportment
    cleanup of comportment in turn 0 and 1
    removal of grid and other useless attributes

--- bots/victor.py
+++ bots/victor.py
@@ -1,103 +1,59 @@
 from engine.bot import Bot, Move
 
 
-"""
-self_id     -- your bot id
-width       -- board width
-height      -- board height
-turn        -- current turn number
-positions   -- {bot_id: (x, y)} for every bot still alive
-alive       -- list of bot ids still alive
-walls       -- frozenset of every occupied (x, y) cell, anyone's trail
 
-Up = +y
-Right = +x
-Down = -y
-Left = -x
-"""
 class VictorBot(Bot):
-
-    _grid : dict[tuple[int, int], int] | None = None
-    enemies_last_pos : dict[int, tuple[int, int]] | None = None
-    enemy_direction :dict[int, str] | None = None
-    order : str = 'prepare'
-
-    @classmethod
-    def create_grid(cls, state):
-        cls._grid = {}
-        for x in range(state.width):
-            for y in range(state.height):
-                cls._grid[(x, y)] = 0
-        for cell in state.positions:
-            cls._grid[cell] = 1
-        for cell in state.walls:
-            cls._grid[cell] = 1
-
-    @classmethod
-    def update_grid(cls, state):
-        for cell in state.positions:
-            cls._grid[cell] = 1
-
-    @classmethod
-    def get_grid(cls, state):
-        if cls._grid is None:
-            cls.create_grid(state)
-        return cls._grid
-
-
+    """
+    Will go towards the enemy and avoid clashing cells and single cell paths as much as possible
+    """
     def decide(self, state) -> Move:
         my_pos = state.positions[state.self_id]
-        grid = self.get_grid(state)
         current_positions = state.positions
         enemies = []
-        enemy = None
-
-        def get_distance(entity):
-            enemy_pos = current_positions[entity]
-            distance = abs(enemy_pos[0] - my_pos[0]) + abs(enemy_pos[0] - my_pos[0])
-            return distance
 
         def is_safe(move: Move) -> bool:
             x, y = my_pos[0] + move.dx, my_pos[1] + move.dy
             if not (0 <= x < state.width and 0 <= y < state.height):
-                return False
-            return not bool(grid[(x, y)])
+                return False                                        # if it's outside of bounds : not safe
+            for selected_enemy in enemies:
+                enemy_pos = state.positions[selected_enemy]
+                if abs(x - enemy_pos[0])+abs(y - enemy_pos[1])==1:
+                    return False                                    # if it's a cell an enemy can reach : not safe
+            return (x, y) not in state.walls                        # if it's a wall : not safe
 
-        def is_dangerous(move: Move) -> bool:
+        def is_safe_path(move: Move) -> bool:
             x, y = my_pos[0] + move.dx, my_pos[1] + move.dy
+            # if the path taken is only 1 cell wide, it's not safe
             if move.dy:
-                if grid[(x + 1, y)] and grid[(x - 1, y)]:
+                if ((x + 1, y) in state.walls or x+1 >= state.width) and ((x - 1, y) in state.walls or x-1 < 0):
                     return False
             else:
-                if grid[(x, y + 1)] and grid[(x, y - 1)]:
+                if ((x, y+1) in state.walls or y+1 >= state.height) and ((x, y-1) in state.walls or y-1 < 0):
                     return False
             return True
 
         def safe_move() -> Move:
-            if get_distance(enemy) <=4:
-                if current_positions[enemy][1] == my_pos[1]:
-                    return Move.UP if is_safe(Move.UP) else Move.DOWN
-                if current_positions[enemy][0] == my_pos[0]:
-                    return Move.LEFT if is_safe(Move.LEFT) else Move.RIGHT
+            # will be used when no path towards the enemy is safe.
+            # we check every move possible, and how safe it is, then return the move
             safe_moves = []
             for move in (Move.UP, Move.RIGHT, Move.DOWN, Move.LEFT):
                 if is_safe(move):
                     safe_moves.append(move)
             for move in safe_moves:
-                if not is_dangerous(move):
+                if is_safe_path(move):
                     return move
-            return safe_moves[0]
+            return safe_moves[0] if safe_moves else Move.UP
 
         def _step_toward(position, target):
             dx = target[0] - position[0]
             dy = target[1] - position[1]
-            if dx > 0 and is_safe(Move.RIGHT):
+            if dx > 0 and is_safe(Move.RIGHT) and is_safe_path(Move.RIGHT):
                 return Move.RIGHT
-            if dx < 0 and is_safe(Move.LEFT):
+            if dx < 0 and is_safe(Move.LEFT) and is_safe_path(Move.LEFT):
                 return Move.LEFT
-            if dy > 0 and is_safe(Move.DOWN):
+            if dy > 0 and is_safe(Move.DOWN) and is_safe_path(Move.DOWN):
                 return Move.DOWN
-            if dy < 0 and is_safe(Move.UP):
+            if dy < 0 and is_safe(Move.UP) and is_safe_path(Move.UP):
                 return Move.UP
             return safe_move()
 
@@ -107,41 +63,4 @@ class VictorBot(Bot):
 
         enemy = enemies[0]
 
-        # if VictorBot.enemies_last_pos:
-        #     for enemy in enemies:
-        #         current = current_positions[enemy]
-        #         last = VictorBot.enemies_last_pos[enemy]
-        #         if current[0] == last[0]:
-        #             # vertical
-        #             if current[1] > last[1]:
-        #                 # down
-        #                 direction = 'down'
-        #             else:
-        #                 # up
-        #                 direction = 'up'
-        #         else:
-        #             # horizontal
-        #             if current[0] > last[0]:
-        #                 # right
-        #                 direction = 'right'
-        #             else:
-        #                 #left
-        #                 direction = 'left'
-        #         VictorBot.enemy_direction[enemy] = direction
-
-        if state.turn != 0:
-             self.update_grid(state)
-
-        VictorBot.enemies_last_pos = current_positions
-
-        if state.turn == 0 or state.turn == 1:
-
-            if state.turn == 0:
-                if current_positions[enemy][0] < my_pos[0]:
-                    return Move.LEFT if is_safe(Move.LEFT) else safe_move()
-                else:
-                    return Move.RIGHT if is_safe(Move.RIGHT) else safe_move()
-
-        if get_distance(enemy) > 4:
-            return _step_toward(my_pos ,current_positions[enemy])
-        return safe_move()
+        return _step_toward(my_pos ,current_positions[enemy])
